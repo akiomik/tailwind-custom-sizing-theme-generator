8e25722dea238caa457b32b562b58480777bb8a2
Akiomi Kamakura <akiomik@gmail.com>
2022-07-05 18:51:54 +0900

    Add footer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -6,4 +6,10 @@
   class="rounded-lg shadow-lg z-10 bg-white md:container md:mx-auto md:max-w-screen-lg p-8 text-slate-900"
 >
   <slot />
+
+  <div class="mt-4">
+    &copy; 2022 AKiomi Kamakura
+    <span class="mx-2 text-slate-200 select-none cursor-default">⚫︎</span>
+    <a href="https://github.com/akiomik/tailwind-custom-sizing-theme-generator">Source</a>
+  </div>
 </main>
