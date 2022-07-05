b51115851a7dedf5d2bbf15d3f9ec1c41d58926e
Akiomi Kamakura <akiomik@gmail.com>
2022-07-05 17:59:36 +0900

    Fix module import

--- src/helpers/CustomThemeGenerator.ts
+++ src/helpers/CustomThemeGenerator.ts
@@ -1,4 +1,4 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
+import defaultTheme from 'tailwindcss/defaultTheme.js';
 
 import { isUnit } from '../entities/Unit';
 import type { Unit } from '../entities/Unit';
